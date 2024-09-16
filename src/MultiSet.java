713d67771a4a0af58357a5528a8697ea786468f9
Richard <wangyc2005@gmail.com>
2024-09-16 16:13:00 -0400

    aaa

--- src/MultiSet.java
+++ src/MultiSet.java
@@ -9,6 +9,12 @@ public abstract class MultiSet {
      * @param item the item to add
      */
     abstract void add(Integer item);
+    abstract void remove(Integer item);
+    abstract boolean contains(Integer item);
+    abstract boolean is_empty();
+    abstract int  count(Integer item);
+    abstract int size(Integer item);
 
-    // TODO finish adding abstract methods to fully describe what it means to be a MultiSet.
+
+
 }
